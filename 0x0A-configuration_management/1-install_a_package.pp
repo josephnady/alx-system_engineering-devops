8cb76ca2cd961d87b1df3474ead2bf8439c51b5a
josephnady <jozephnady@gmail.com>
2024-05-04 15:58:49 -0700

    install package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Define package resource to install Flask
-package { 'flask':  
-  provider        => 'pip3',
-  install-options => '2.1.0'
+package { 'flask':
+  provider => 'pip3',
+  command  => '/usr/bin/pip3 install flask==2.1.0',
 }
