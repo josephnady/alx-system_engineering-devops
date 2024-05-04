56b461f004b6568974040e867563ddcdb3366f2c
josephnady <jozephnady@gmail.com>
2024-05-04 16:15:05 -0700

    install package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,26 @@
+# File: install_python_flask_werkzeug.pp
+
+# Define package resource to install Python3
+package { 'python3':
+  ensure => installed,
+}
+
+# Define package resource to install python3-pip
+package { 'python3-pip':
+  ensure => installed,
+  require => Package['python3'], # Ensure Python3 is installed before installing python3-pip
+}
+
 # Define package resource to install Flask
-package { 'python3-flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure python3-pip is installed before installing Flask
+}
+
+# Define package resource to install Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure python3-pip is installed before installing Werkzeug
 }
